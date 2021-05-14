f9fa9a73d7ed4bcdbb16fc2a1152d54cc9c8091d
Alex Babou <ababou@dendev.net>
2021-05-14 17:08:55 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alex Babou on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
